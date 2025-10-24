5f43d816350da68e2e6746dd9ed78a07130d825d
ANETI-Tunnisia <hamdi_ammar@live.com>
2025-10-24 23:17:00 +0100

    #3

--- ng-Analog-Clock-App-Tester/src/app/app.ts
+++ ng-Analog-Clock-App-Tester/src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnalogClock } from '@myangularapps/analog-clock';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [AnalogClock, RouterOutlet],
+  schemas: [NO_ERRORS_SCHEMA],
+  template: `<analog-clock [options]="{ size: 200 }"></analog-clock>`,
 })
 export class App {
   protected readonly title = signal('ng-Analog-Clock-App-Tester');
